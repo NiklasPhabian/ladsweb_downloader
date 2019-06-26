4c1d512c8864c3433fface30de2edecd3c9dfb06
Niklas <griessbaum@gmail.com>
2019-06-26 15:45:36 -0700

    added parallel download

--- ladsweb_file.py
+++ ladsweb_file.py
@@ -6,6 +6,9 @@ import urllib
 import wget
 import requests
 import time
+from multiprocessing.pool import Pool
+
+
 
 
 class LadswebFile:
@@ -18,6 +21,7 @@ class LadswebFile:
             self.get_url()
         self.get_file_name()        
         self.checksum = None
+        self.header = None
         self.properties = {}        
     
     def get_url(self):        
@@ -36,6 +40,37 @@ class LadswebFile:
             key = child.tag.split('}')[1]
             value = child.text            
             self.properties[key] = value     
+            
+    def get_header(self):
+        r = requests.head(self.url) 
+        self.header = r.headers
+        
+    def file_size(self):
+        if self.header is None:
+            self.get_header()
+        return int(self.header['content-length'])         
+            
+    def download_chunk_urllib(self, ranges):        
+        req = urllib.request.Request(self.url)
+        req.headers['Range'] = 'bytes=%s-%s' % (ranges[0], ranges[1])        
+        return urllib.request.urlopen(req).read()
+    
+    def download_chunk_requests(self, ranges):
+        headers = {'Range': 'bytes=%d-%d' % (ranges[0], ranges[1])} 
+        r = requests.get(self.url, headers=headers, stream=True) 
+        return r.content        
+    
+    def download_parallel(self, file_path, threads=16):        
+        chunk_size = int(self.file_size() / threads ) + 1   
+        ranges = []
+        for start in range(0, self.file_size(), chunk_size):    
+            end = start + chunk_size-1
+            ranges.append((start, end))
+        with Pool(threads) as p:
+            chunks = p.map(self.download_chunk_requests, ranges)
+        with open(file_path, 'wb') as out_file:
+            for chunk in chunks:
+                out_file.write(chunk)    
      
     def download_urllib(self, file_path):
         urllib.request.urlretrieve(self.url, filename=file_path)                        
@@ -51,7 +86,7 @@ class LadswebFile:
     def download(self, folder):
         file_path = folder + '/' + self.file_name        
         try:
-            self.download_requests(file_path)        
+            self.download_parallel(file_path)        
         except Exception as e:
             print(e)
             print('download failed, trying again')
