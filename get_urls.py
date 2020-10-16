ac6ff5180421c0060df7b3fde8433cda06d3bab5
griessbaum <griessbaum@ucsb.edu>
2020-10-16 13:48:19 -0300

    added day/night flag

--- get_urls.py
+++ get_urls.py
@@ -8,7 +8,7 @@ import argparse
 from eta import ETA
 
 
-def get_urls_long(product, collection, start, stop, bbox):
+def get_urls_long(product, collection, start, stop, bbox, day, night):
     # The api times out for ranges of more than a couple of months worth of tiles
     # Therefore we make iterative calls to the api
     urls = []
@@ -17,25 +17,27 @@ def get_urls_long(product, collection, start, stop, bbox):
     iterator = start    
     step = datetime.timedelta(days=100)
     while iterator + step < stop:
-        urls += get_urls(product=product, collection=collection, start=iterator, stop=iterator+step, bbox=bbox)
+        urls += get_urls(product=product, collection=collection, 
+                         start=iterator, stop=iterator+step, bbox=bbox, day=day, night=night)
         iterator += step
     urls += get_urls(product=product, collection=collection, start=iterator, stop=stop, bbox=bbox)    
     return urls
 
 
-def get_urls(product, collection, start, stop, bbox):
+def get_urls(product, collection, start, stop, bbox, day=True, night=False):
     host = 'https://ladsweb.modaps.eosdis.nasa.gov'
     api = '/api/v1/files/'
     query = 'product={product}&collection={collection}&'\
             'dateRanges={start}..{stop}&'\
             'areaOfInterest=x{west}y{north},x{east}y{south}&'\
-            'nightCoverage=true'
+            'nightCoverage={night}&dayCoverage={day}'
     query = query.format(product=product, collection=collection,
                          start=start, stop=stop,
-                         north=bbox['north'], south=bbox['south'], west=bbox['west'], east=bbox['east'])
+                         north=bbox['north'], south=bbox['south'], west=bbox['west'], east=bbox['east'],
+                         day=str(day), night=str(night))
     print(host+api+query)
     ret = requests.get(host+api+query, headers={'X-Requested-With': 'XMLHttpRequest'}, timeout=600)   
-    if len(ret.json()) == 0:
+    if ret.status_code!=200 or len(ret.json()) == 0:
         print('empty response; flasely specified arguments?')
         return []
     urls = []    
@@ -51,15 +53,19 @@ if __name__ == '__main__':
     bbox.read('bbox.config')
     parser = argparse.ArgumentParser(description='Downloads file list from Ladsweb')
     parser.add_argument('--product', metavar='product', nargs='?', type=str, 
-                        help='ladsweb product (e.g. VNP02DNB, VNP03DNB, CLDMSK_L2_VIIRS_SNPP, VNP46A1)')
+                        help='ladsweb product (e.g. VNP02DNB, VNP03DNB, CLDMSK_L2_VIIRS_SNPP, VNP46A1, MOD09)')
     parser.add_argument('--collection', metavar='collection', nargs='?', type=str, 
-                        help='ladsweb collection (e.g. 5110, 5000)')
+                        help='ladsweb collection (e.g. 5110, 5000, 6)')
     parser.add_argument('--region', metavar='region', nargs='?', type=str, 
                         help='region as specified in bbox.config', choices=bbox.sections())    
     parser.add_argument('--start', metavar='start', nargs='?', type=str, 
                         help='start date (yyyy-mm-dd)', default='2019-04-01')
     parser.add_argument('--stop', metavar='stop', nargs='?', type=str, 
                         help='stop date (yyyy-mm-dd)', default='2019-09-01')
+    parser.add_argument('--day', dest='day', action='store_true')
+    parser.add_argument('--night', dest='night', action='store_true')
+    parser.set_defaults(day=True)
+    parser.set_defaults(night=False)
 
     args = parser.parse_args()   
     if args.product is None or args.region is None or args.collection is None:
@@ -70,7 +76,8 @@ if __name__ == '__main__':
     urls = get_urls_long(product=args.product, 
                          collection=args.collection, 
                          start=args.start, stop=args.stop, 
-                         bbox=bbox[args.region])
+                         bbox=bbox[args.region],
+                         day=args.day, night=args.night)
 
     file_list_name = 'file_lists/{region}_{product}.csv'.format(region=args.region, product=args.product)
     with open(file_list_name, 'a') as files_log:        
